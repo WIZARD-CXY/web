941afc0f0401458ae51112eb27c558f5910a3c66
yzd <refyzd@gmail.com>
2012-09-25 18:44:08 +0800

    Correct some mistakes

--- js/client.js
+++ js/client.js
@@ -1,22 +1,10 @@
 var xmlHttp
 function startMotion(value)
 {
-if (str.length==0)
-  {
-  document.getElementById("state").innerHTML=""
-  }
 xmlHttp=new XMLHttpRequest()
 url="/cgi-bin/startMotion.py"
-str=str*4+300
+value=value*4+300
 url=url+"?motiondata="+"500"+value
-xmlHttp.onreadystatechange=stateChanged
 xmlHttp.open("get",url,true)
 xmlHttp.send()
-}
-function stateChanged()
-{
-if (xmlHttp.readyState==4 || xmlHttp.readyState=="complete")
- {
- document.getElementById("state").innerHTML=xmlHttp.responseText
- }
 }
