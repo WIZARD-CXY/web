4dc46fc203f2ba099f8cdc5c588f152729f5d71b
wizard <wizard_cxy@hotmail.com>
2012-10-02 19:50:40 +0800

    fix some bugs

--- js/client.js
+++ js/client.js
@@ -2,7 +2,7 @@ var xmlHttp
 function startMotion(value)
 {
 xmlHttp=new XMLHttpRequest()
-url="/cgi-bin/startMotion.py"
+url="/cgi-bin/startMotion.cgi"
 value=value*4+300
 url=url+"?motiondata="+"500"+value
 xmlHttp.open("get",url,true)
